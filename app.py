2b459ea8029ce0074d718e431c9ac3624e6fcbd3
João Pedro Longo Zucoloto <49160383+JoaoPedroZucoloto@users.noreply.github.com>
2020-07-23 15:20:53 -0300

    teste02

--- app.py
+++ app.py
@@ -4,7 +4,17 @@ from flask import render_template
 import sys, os, time
 import streamlit as st
 
-st.title('Oi')
+st.title('Roi')
 
-challenge       = request.args.get('hub.challenge',    default = '*', type = str)
-verify_token    = request.args.get('hub.verify_token', default = '',  type = str)
+application = Flask(__name__)
+
+@application.route('https://davi-chatbot-jpz.herokuapp.com/', methods=['GET', 'POST'])
+def webhook():    
+    challenge       = request.args.get('hub.challenge',    default = '*', type = str)
+    verify_token    = request.args.get('hub.verify_token', default = '',  type = str)
+    if challenge != '*' and verify_token == 'token-da-loucura':
+        return challenge
+       
+    data = request.data.decode('utf-8')
+        
+    return 'Oi :)'
