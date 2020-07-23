673b887a34b4d3a03ca36939dddcd9e7ce761ac0
João Pedro Longo Zucoloto <49160383+JoaoPedroZucoloto@users.noreply.github.com>
2020-07-23 15:01:03 -0300

    coticu

--- app.py
+++ app.py
@@ -1,19 +1,7 @@
-import pandas as pd
-import numpy as np
-import streamlit as st
-import sys, os, time
 from flask import Flask
 from flask import request
 from flask import render_template
+import sys, os, time
+import streamlit as st
 
-def main(): 
-    challenge       = request.args.get('hub.challenge',    default = '*', type = str)
-    verify_token    = request.args.get('hub.verify_token', default = '',  type = str)
-    if challenge != '*' and verify_token == 'aqui vai o token que tu configura no Workplace':
-        print(challenge)
-
-    data = request.data.decode('utf-8')
-    st.write(data)
-
-if __name__ == '__main__':
-    main()
+st.title('Oi')
