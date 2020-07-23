c26494f097e1b5e7f1f875c08fe1b8ed44c2df08
João Pedro Longo Zucoloto <49160383+JoaoPedroZucoloto@users.noreply.github.com>
2020-07-23 17:04:36 -0300

    teste15

--- app.py
+++ app.py
@@ -4,19 +4,20 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/webhook', methods=['GET', 'POST'])
 def nao_entre_em_panico():
+    print(request.json)
     challenge       = request.args.get('hub.challenge',    default = '*', type = str)
     verify_token    = request.args.get('hub.verify_token', default = '',  type = str)
     if challenge != '*' and verify_token == 'chupacabra':
-        return 'Entrei'
+        return challenge
     else:
-        return 'Não Entrei'
+        return challenge
        
     data = request.data.decode('utf-8')
     print(data)
         
-    return data
+    return challenge
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
