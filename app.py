26c90df2250b4047ce2e876bfaaceddd551c64c1
João Pedro Longo Zucoloto <49160383+JoaoPedroZucoloto@users.noreply.github.com>
2020-07-23 17:08:20 -0300

    teste17

--- app.py
+++ app.py
@@ -10,14 +10,14 @@ def nao_entre_em_panico():
     challenge       = request.args.get('hub.challenge',    default = '*', type = str)
     verify_token    = request.args.get('hub.verify_token', default = '',  type = str)
     if challenge != '*' and verify_token == 'chupacabra':
-        return challenge
+        return challenge, verify_token
     else:
-        return challenge
+        return challenge, verify_token
        
     data = request.data.decode('utf-8')
     print(data)
         
-    return challenge
+    return challenge, verify_token
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
