8ef0efb0171149d2b35a6c9370483632f21b6650
João Pedro Longo Zucoloto <49160383+JoaoPedroZucoloto@users.noreply.github.com>
2020-07-23 16:20:18 -0300

    teste10

--- app.py
+++ app.py
@@ -4,10 +4,12 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def nao_entre_em_panico():
     ar = 'Lente Azul'
-    return ar
+    challenge       = request.args.get('hub.challenge',    default = '*', type = str)
+    verify_token    = request.args.get('hub.verify_token', default = '',  type = str)
+    return ar, challenge, verify_token
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
