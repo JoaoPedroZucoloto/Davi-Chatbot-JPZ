78cc4ceee767b7c2c21fe7ee81a5ba032a4f42b5
João Pedro Longo Zucoloto <49160383+JoaoPedroZucoloto@users.noreply.github.com>
2020-07-23 16:42:09 -0300

    toneladas1

--- app.py
+++ app.py
@@ -8,13 +8,13 @@ app = Flask(__name__)
 def nao_entre_em_panico():
     challenge       = request.args.get('hub.challenge',    default = '*', type = str)
     verify_token    = request.args.get('hub.verify_token', default = '',  type = str)
-    if challenge != '*' and verify_token == 'aqui vai o token que tu configura no Workplace':
+    if challenge != '*' and verify_token == 'chupacabra':
         return challenge
        
     data = request.data.decode('utf-8')
     print(data)
         
-    return 'Lente Azul'
+    return data
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
