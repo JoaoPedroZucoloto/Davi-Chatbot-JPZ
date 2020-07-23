47a992d09c6781a07402353516ff6879bce3cc45
João Pedro Longo Zucoloto <49160383+JoaoPedroZucoloto@users.noreply.github.com>
2020-07-23 16:27:43 -0300

    teste11

--- app.py
+++ app.py
@@ -9,7 +9,11 @@ def nao_entre_em_panico():
     ar = 'Lente Azul'
     challenge       = request.args.get('hub.challenge',    default = '*', type = str)
     verify_token    = request.args.get('hub.verify_token', default = '',  type = str)
-    return ar, challenge, verify_token
+    if challenge != '*' and verify_token == 'aqui vai o token que tu configura no Workplace':
+        return challenge
+    
+    return ar
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
