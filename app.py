98dcdf81bb4eb6d3b38236da661c278f52e9dee3
João Pedro Longo Zucoloto <49160383+JoaoPedroZucoloto@users.noreply.github.com>
2020-07-23 17:39:11 -0300

    ghj

--- app.py
+++ app.py
@@ -4,14 +4,7 @@ app = Flask(__name__)
 
 VERIFY_TOKEN = "chupacabra"
 
-@app.route('/webhook', methods=['GET'])
+@app.route('/', methods=['GET'])
 def index():
     return "This is a Facebook Messenger test bot server."
 
-
-@app.route('/webhook', methods=['GET'])
-def verify():
-    # webhook verification
-    if request.args.get('hub.mode') == 'subscribe' and request.args.get('hub.challenge'):
-        if request.args.get('hub.verify_token') == VERIFY_TOKEN:
-            return 'Verification token missmatch', 403
