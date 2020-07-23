533860d16f5d904f019ffe0e06f31e7cbbcbb9f6
João Pedro Longo Zucoloto <49160383+JoaoPedroZucoloto@users.noreply.github.com>
2020-07-23 17:26:39 -0300

    grr

--- app.py
+++ app.py
@@ -5,21 +5,15 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 @app.route('/webhook', methods=['GET', 'POST'])
-def nao_entre_em_panico():
-    def api_response():
-        from flask import jsonify
-        
-        if request.method == 'POST':
-            return jsonify(**request.json)
-    
+def nao_entre_em_panico():  
     challenge       = request.args.get('hub.challenge',    default = '*', type = str)
     verify_token    = request.args.get('hub.verify_token', default = '',  type = str)
     print(type(challenge))
     print(type(verify_token))
     if challenge != '*' and verify_token == 'chupacabra':
-        return challenge, verify_token
+        return challenge
     else:
-        return challenge, verify_token
+        return challenge
        
     data = request.data.decode('utf-8')
     print(data)
