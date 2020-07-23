d808562c7a7e38538abe04cffa7f110b31191d65
João Pedro Longo Zucoloto <49160383+JoaoPedroZucoloto@users.noreply.github.com>
2020-07-23 15:58:51 -0300

    teste07

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def nao_entre_em_panico():
     if request.headers.get('Authorization') == '42':
         return jsonify({"42": "a resposta para a vida, o universo e tudo mais"})
-    return jsonify({"message": "Não entre em pânico!"})
+    print('lente azul')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
