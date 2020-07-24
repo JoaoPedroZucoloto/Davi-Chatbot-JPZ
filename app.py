4c17a5598ac027838525f43224c6632ef2ec054e
João Pedro Longo Zucoloto <49160383+JoaoPedroZucoloto@users.noreply.github.com>
2020-07-24 13:33:35 -0300

    oitre

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-import os, sys
-from flask import Flask, request
+import os
+from flask import Flask, jsonify, request
+
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
-def verify(): 
-    return 'ok'
+@app.route('/', methods=['GET', 'POST'])
+def nao_entre_em_panico():
+    ar = 'Lente Azul'
+    challenge       = request.args.get('hub.challenge',    default = '*', type = str)
+    verify_token    = request.args.get('hub.verify_token', default = '',  type = str)
+    if challenge != '*' and verify_token == 'aqui vai o token que tu configura no Workplace':
+        return challenge
+    
+    return ar, challenge
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
