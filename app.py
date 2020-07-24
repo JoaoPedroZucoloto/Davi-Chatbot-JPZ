93c7649012d4c4f648eaede8d0479632bf18f457
João Pedro Longo Zucoloto <49160383+JoaoPedroZucoloto@users.noreply.github.com>
2020-07-24 13:57:08 -0300

    ptresa

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def nao_entre_em_panico():
     ar = 'Lente Azul'
-    challenge       = request.args.get('hub.challenge',    default = '*', type = str)
+    challenge       = request.args.get('hub.challenge', type = str)
     verify_token    = request.args.get('hub.verify_token', default = '',  type = str)
     if challenge != '*' and verify_token == 'aqui vai o token que tu configura no Workplace':
         return challenge
