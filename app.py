6a09739019064c1421df4abbe5adecbf288ebf4e
João Pedro Longo Zucoloto <49160383+JoaoPedroZucoloto@users.noreply.github.com>
2020-07-24 16:39:41 -0300

    1525tp47

--- app.py
+++ app.py
@@ -18,9 +18,13 @@ def nao_entre_em_panico():
 
 @app.route("/webhook", methods=['GET','POST'])
 def webhook_handle():
-    output = request.get_json()
-    print(output)
-    return 'ok' + str(output)
+    ar = 'Lente Azul'
+    challenge       = request.args.get('hub.challenge',    default = '*', type = str)
+    verify_token    = request.args.get('hub.verify_token', default = '',  type = str)
+    if challenge != '*' and verify_token == 'chupacabra':
+        return challenge
+
+    return 'ok' + challenge
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
