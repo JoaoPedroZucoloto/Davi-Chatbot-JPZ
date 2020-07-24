6ca68ac258adf93f7ed38cfdb7265b75bc0078e5
João Pedro Longo Zucoloto <49160383+JoaoPedroZucoloto@users.noreply.github.com>
2020-07-24 11:38:43 -0300

    qwerf

--- app.py
+++ app.py
@@ -18,5 +18,4 @@ def nao_entre_em_panico():
     return data
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
