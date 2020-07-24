2d47a5a40affdd44e2a0138ed71f00ae0b88f8a7
João Pedro Longo Zucoloto <49160383+JoaoPedroZucoloto@users.noreply.github.com>
2020-07-24 14:20:07 -0300

    presazxhjkjhgf

--- app.py
+++ app.py
@@ -1,21 +1,10 @@
 import os
 from flask import Flask, jsonify, request
 
-def pretty_print_POST(req):
-    """
-    At this point it is completely built and ready
-    to be fired; it is "prepared".
-
-    However pay attention at the formatting used in 
-    this function because it is programmed to be pretty 
-    printed and may differ from the actual request.
-    """
-    print('{}\n{}\r\n{}\r\n\r\n{}'.format(
-        '-----------START-----------',
-        req.method + ' ' + req.url,
-        '\r\n'.join('{}: {}'.format(k, v) for k, v in req.headers.items()),
-        req.body,
-    ))
+def api_response():
+    from flask import jsonify
+    if request.method == 'POST':
+        return jsonify(**request.json)
 
 app = Flask(__name__)
 
@@ -26,9 +15,9 @@ def nao_entre_em_panico():
     verify_token    = request.args.get('hub.verify_token', default = '',  type = str)
     if challenge != '*' and verify_token == 'aqui vai o token que tu configura no Workplace':
         return challenge
-    response = requests.post('https://davi-chatbot-jpz.herokuapp.com/', data={'key1':'value1'})
+    
     data = request.data.decode('utf-8')
-    return response.request.body
+    return api_response(data)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
