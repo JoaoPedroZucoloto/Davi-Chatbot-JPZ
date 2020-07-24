2129c88c103b379bd89260965827bb2c93941b70
João Pedro Longo Zucoloto <49160383+JoaoPedroZucoloto@users.noreply.github.com>
2020-07-24 15:09:28 -0300

    asdf7

--- app.py
+++ app.py
@@ -19,6 +19,12 @@ def nao_entre_em_panico():
     data = request.data.decode('utf-8')
     return data
 
+@app.route("/webhook", methods=['POST'])
+def webhook_handle():
+    output = request.get_json()
+    print(output)
+    return 'ok'
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
