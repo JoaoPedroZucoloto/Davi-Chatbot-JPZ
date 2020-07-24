756faecefe8e4836f60cb1bf6e2ecf433b8e899f
João Pedro Longo Zucoloto <49160383+JoaoPedroZucoloto@users.noreply.github.com>
2020-07-24 11:51:05 -0300

    poqwe

--- app.py
+++ app.py
@@ -3,12 +3,13 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/webhook', methods=['GET', 'POST'])
 def verify_webhook(req):
     if req.args.get("hub.verify_token") == VERIFY_TOKEN:
         return req.args.get("hub.challenge")
     else:
         return "incorrect"
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
