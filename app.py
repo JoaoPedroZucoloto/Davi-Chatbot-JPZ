b456c108f0f85fa979de61b2eeb97613d58af0af
João Pedro Longo Zucoloto <49160383+JoaoPedroZucoloto@users.noreply.github.com>
2020-07-24 12:22:30 -0300

    oi

--- app.py
+++ app.py
@@ -5,11 +5,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def verify(): 
-    if request.args.get('hub.mode') == 'subscribe' and request.args.get('hub.challenge'):
-        if not request.args.get("hub.verify_token") == "chupacabra":
-            return "Ok", 403
-        return request.args['hub.challenge'], 200
-    return "Fela", 200
+    return 'ok'
 
 if __name__ == "__main__":
     app.run(debug = True)
