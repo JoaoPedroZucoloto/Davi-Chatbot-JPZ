5e5c4b5cfadb80e7cbbb7f65a681a8f16ed80aca
João Pedro Longo Zucoloto <49160383+JoaoPedroZucoloto@users.noreply.github.com>
2020-07-24 12:11:46 -0300

    kdjf

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
-from flask import Flask, request
+import os, sys
+from flask import Flask, reqest
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
-def webhook():
-    print(request.data)
-    data = request.get_json()
-    return 'ok'
-    
+@app.route('/', methods=['GET'])
+def verify(): 
+    if request.args.get('hub.mode') == 'subscribe' and request.args.get('hub.challenge'):
+        if not request.args.get("hub.verify_token") == "chupacabra":
+            return "Ok", 403
+        return request.args['hub.challenge'], 200
+    return "Fela", 200
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug = True)
