31430b09d4f49c92e25232a6bafbc1a88072b24c
João Pedro Longo Zucoloto <49160383+JoaoPedroZucoloto@users.noreply.github.com>
2020-07-24 14:01:35 -0300

    predfvc

--- app.py
+++ app.py
@@ -1,19 +1,34 @@
 import os
 from flask import Flask, jsonify, request
 
+def pretty_print_POST(req):
+    """
+    At this point it is completely built and ready
+    to be fired; it is "prepared".
+
+    However pay attention at the formatting used in 
+    this function because it is programmed to be pretty 
+    printed and may differ from the actual request.
+    """
+    print('{}\n{}\r\n{}\r\n\r\n{}'.format(
+        '-----------START-----------',
+        req.method + ' ' + req.url,
+        '\r\n'.join('{}: {}'.format(k, v) for k, v in req.headers.items()),
+        req.body,
+    ))
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def nao_entre_em_panico():
     ar = 'Lente Azul'
-    challenge       = request.args.get('hub.challenge', type = str)
+    challenge       = request.args.get('hub.challenge',    default = '*', type = str)
     verify_token    = request.args.get('hub.verify_token', default = '',  type = str)
     if challenge != '*' and verify_token == 'aqui vai o token que tu configura no Workplace':
         return challenge
     
     data = request.data.decode('utf-8')
-    return challenge
+    return pretty_print_POST(request)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
